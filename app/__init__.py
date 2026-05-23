4d1dd1f06eebce36f86a102b080a41d27e71fddd
karaship01 <karaship.01@gmail.com>
2026-05-24 02:23:21 +0300

    Sprint 2: Bootstrap 5 altyapısı kuruldu ve base.html modern arayüze geçirildi

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+from flask_bootstrap import Bootstrap5
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
@@ -6,6 +7,7 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
+bootstrap = Bootstrap5(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
